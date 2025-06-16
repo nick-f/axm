0d8b832b78cd69cec72bc7929da8b2a26e9a1d92
nick-f <nick@nickf.dev>
2025-06-16 21:51:50 +1000

    Use proper variable names for client in spec

--- spec/axm/client_spec.rb
+++ spec/axm/client_spec.rb
@@ -3,7 +3,7 @@
 require "axm"
 
 RSpec.describe Axm::Client do
-  let(:subject) do
+  let(:client) do
     described_class.new(
       token: "a-valid-token",
       org_type: "business"
@@ -11,18 +11,18 @@ RSpec.describe Axm::Client do
   end
 
   describe "when the token is not provided" do
-    let(:subject) do
+    let(:client) do
       described_class.new(
         token: nil,
         org_type: "business"
       )
     end
 
-    it { expect { subject }.to raise_error(ArgumentError, "Provide the token argument") }
+    it { expect { client }.to raise_error(ArgumentError, "Provide the token argument") }
   end
 
   describe "when the org_type is an invalid value" do
-    let(:subject) do
+    let(:client) do
       described_class.new(
         token: "a-token-value",
         org_type:
@@ -32,7 +32,7 @@ RSpec.describe Axm::Client do
     let(:org_type) { "retail" }
 
     it "raises an error message" do
-      expect { subject }.to raise_error(
+      expect { client }.to raise_error(
         ArgumentError,
         "Provide a valid org_type argument ('business' for Apple Business Manager or 'school' for Apple School Manager) or the api_endpoint argument for your environment"
       )
@@ -40,7 +40,7 @@ RSpec.describe Axm::Client do
   end
 
   describe "when the org_type is not provided" do
-    let(:subject) do
+    let(:client) do
       described_class.new(
         token: "a-token-value",
         org_type: nil,
@@ -52,7 +52,7 @@ RSpec.describe Axm::Client do
       let(:api_endpoint) { nil }
 
       it "raises an error message" do
-        expect { subject }
+        expect { client }
           .to raise_error(
             ArgumentError,
             "Provide a valid org_type argument ('business' for Apple Business Manager or 'school' for Apple School Manager) or the api_endpoint argument for your environment"
@@ -62,7 +62,7 @@ RSpec.describe Axm::Client do
   end
 
   describe "when the org_type is a valid value" do
-    let(:subject) do
+    let(:client) do
       described_class.new(
         token: "a-token-value",
         org_type:,
@@ -75,13 +75,13 @@ RSpec.describe Axm::Client do
     describe "and the org_type is 'business'" do
       let(:org_type) { "business" }
 
-      it { expect { subject }.not_to raise_error }
+      it { expect { client }.not_to raise_error }
     end
 
     describe "and the org_type is 'school'" do
       let(:org_type) { "school" }
 
-      it { expect { subject }.not_to raise_error }
+      it { expect { client }.not_to raise_error }
     end
 
     describe "and the api_endpoint is provided" do
@@ -89,7 +89,7 @@ RSpec.describe Axm::Client do
       let(:api_endpoint) { "https://api-business.apple.com/api/v1" }
 
       it "raises an error message" do
-        expect { subject }
+        expect { client }
           .to raise_error(
             ArgumentError,
             "Provide either a valid org_type or api_endpoint argument for your environment, not both"
