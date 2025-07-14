0a94cfcb7dbc4b8a905df4c4dd43a43bc88515dd
nick-f <nick@nickf.dev>
2025-07-14 22:27:05 +1000

    Add Read device information method

--- lib/axm/client/organization_devices.rb
+++ lib/axm/client/organization_devices.rb
@@ -13,6 +13,18 @@ module Axm
       def list_org_devices(options = {})
         get("v1/orgDevices", options)
       end
+
+      # Retrieves information about a specific device in the organization.
+      #
+      # @param options [Hash] Optional query parameters to filter or paginate results.
+      #   - fields: (Array) Array of fields to include in the response.
+      # @return [<Hash>] A device and its selected attributes.
+      #
+      # See: https://developer.apple.com/documentation/applebusinessmanagerapi/get-orgdevice-information
+      # See: https://developer.apple.com/documentation/appleschoolmanagerapi/get-orgdevice-information
+      def device(id, options = {})
+        get("/v1/orgDevices/#{id}", options)
+      end
     end
   end
 end
