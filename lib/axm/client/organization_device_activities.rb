5abd70cd8d891026d482d611e634b3832a45b9b8
nick-f <nick@nickf.dev>
2025-07-15 22:02:18 +1000

    Add ability to assign devices to MDM servers

--- lib/axm/client/organization_device_activities.rb
+++ lib/axm/client/organization_device_activities.rb
@@ -12,6 +12,40 @@ module Axm
       def org_device_activity(activity_id, options = {})
         get("v1/orgDeviceActivities/#{activity_id}", options)
       end
+
+      # Assigns a device to an MDM server.
+      #
+      # @param device_id [String] The unique identifier of the device to be assigned.
+      # @param mdm_server_id [String] The unique identifier of the MDM server to which the device will be assigned.
+      # @return [Hash, Integer] The response from the POST method, containing details of the assignment and status code.
+      def assign(device_id, mdm_server_id)
+        request_body = {
+          data: {
+            type: "orgDeviceActivities",
+            attributes: {
+              activityType: "ASSIGN_DEVICES"
+            },
+            relationships: {
+              mdmServer: {
+                data: {
+                  type: "mdmServers",
+                  id: mdm_server_id
+                }
+              },
+              devices: {
+                data: [
+                  {
+                    type: "orgDevices",
+                    id: device_id
+                  }
+                ]
+              }
+            }
+          }
+        }
+
+        post("v1/orgDeviceActivities", request_body)
+      end
     end
   end
 end
