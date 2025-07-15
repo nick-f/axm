4bc0a55073f4140e2a629945f53a1555dbc77668
nick-f <nick@nickf.dev>
2025-07-15 21:15:27 +1000

    Change how field keys are determined in most cases

--- lib/axm/client.rb
+++ lib/axm/client.rb
@@ -116,8 +116,10 @@ module Axm
     def get(path, options = {})
       options = options.dup
 
-      endpoint = path.split('/').last
+      # API endpoints are prefixed with 'v1', so we can extract the endpoint from the path.
+      endpoint = path.split('/')[1]
 
+      # For the cases where the fields_key is different to the path component, it can be overriden as an option.
       fields_key = options.delete(:fields_key) || endpoint
 
       fields = options.delete(:fields)
